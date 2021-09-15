dfb7b95bf5f68ec9bba7ec8f6ca8e6c4aa978631
inga.kazlauska <inga.kazlauska@gmail.com>
2021-09-15 21:12:37 +0300

    Changes

---
+++ ConsoleApp3/ConsoleApp3/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace ConsoleApp3
+{
+
+    public class Person
+
+    {
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; }
+
+        private int _age;
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                _age = Math.Abs(value);
+            }
+        }
+
+        public int Age2 { get; set; }
+
+        public double Money { get; set; }
+
+        public bool IsMale { get; set; }
+
+        public char Initial { get; set; }
+
+        public String GetGreeting()
+        {
+            string greeting = "Hello, my name is " + Name + " and I am " + Age + " years old!";
+
+            return greeting;
+
+
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Inga");
+            Person person2 = person;
+
+
+            // person.Name = "Inga123";
+            person.Age = 31;
+            person.IsMale = false;
+            person.Money = 1258.569;
+            person.Initial = 'A';
+
+            person2.Age = 26;   
+
+            string greeting = person.GetGreeting();
+
+            Console.WriteLine(greeting);
+
+        }
+    }
+}
